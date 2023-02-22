c967ebdb77e5ab53c4517fb355c67463b619de12
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-22 08:06:31 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "78ca2d50-4dad-45b0-a238-ff0bcb1d0729",
+    "home_feed": [
+        {
+            "id": "74efba64-75e6-4338-b3d5-113d2ad4426a",
+            "artist_name": "concern",
+            "home_carousel": [
+                {
+                    "id": "0706cbee-23cf-48e2-a972-1b756f6929ef",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                },
+                {
+                    "id": "7f7fc663-a8d7-4a46-845b-f9cb4660a715",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png"
+                },
+                {
+                    "id": "6b39183b-ce24-46f7-94c9-6595d11ab471",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/33.png"
+                },
+                {
+                    "id": "965236a0-bd1c-4911-a820-2fafdf7630f3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/80.png"
+                },
+                {
+                    "id": "1027e9ea-6564-4ab6-b452-2def27da48d1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/199.png"
+                }
+            ],
+            "art_caption": "lawyer",
+            "art_description": "Country lawyer care support impact agent. Close back serious red eat Mr industry."
+        },
+        {
+            "id": "808ad0ad-352f-4ffc-b15f-b03015476ec9",
+            "artist_name": "phone",
+            "home_carousel": [
+                {
+                    "id": "2c4be215-690f-41c0-98a2-b73a7320e4db",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/135.png"
+                },
+                {
+                    "id": "56a0a9ef-b2da-466f-b3a2-e509dd6c6f10",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/78.png"
+                },
+                {
+                    "id": "b7792ce9-70b0-478f-a430-d783670887ca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/334.png"
+                },
+                {
+                    "id": "4f033611-3ca1-4238-bd15-66889a83d020",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png"
+                },
+                {
+                    "id": "24711708-adbe-4241-a5e6-ba0573815d74",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/88.png"
+                }
+            ],
+            "art_caption": "carry",
+            "art_description": "Top seven year. Home beat by trouble best firm. Figure son green issue compare contain rule."
+        },
+        {
+            "id": "c066ccc8-b179-40d9-b067-6b8d52bd0018",
+            "artist_name": "find",
+            "home_carousel": [
+                {
+                    "id": "0fc906c4-afc6-4cf3-8b65-2d3b658df210",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/99.png"
+                },
+                {
+                    "id": "e489d163-875f-47b8-9fc9-e1a9bccbbab2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/187.png"
+                },
+                {
+                    "id": "d082ec54-49e9-45fb-bc34-6b9cf696c557",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                },
+                {
+                    "id": "8cde163a-aaaf-4aea-930e-a45438268509",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/5.png"
+                },
+                {
+                    "id": "5d857b55-5df5-4d3a-b1c6-f23d2b747662",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/239.png"
+                }
+            ],
+            "art_caption": "police",
+            "art_description": "History also condition system. Sound born help pull."
+        },
+        {
+            "id": "bbaf0b39-c2ef-4b85-b142-41d5c8e235db",
+            "artist_name": "girl",
+            "home_carousel": [
+                {
+                    "id": "e62cb741-6533-4632-b663-38c28b459115",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/154.png"
+                },
+                {
+                    "id": "4a6c25ef-879a-4e86-a6b7-00a55eac122b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/350.png"
+                },
+                {
+                    "id": "bbe3d1c0-868a-4f37-965d-2edaec247c79",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png"
+                },
+                {
+                    "id": "ba4dee80-bf40-40ae-9cff-092c05ded23e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/361.png"
+                },
+                {
+                    "id": "73b97376-308b-4a25-b0c8-c991a1d67bf3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/180.png"
+                }
+            ],
+            "art_caption": "because",
+            "art_description": "Town nor American executive eat project tell. According senior individual."
+        },
+        {
+            "id": "7c5a2374-9fbf-451c-b1a9-4ebd67c479fb",
+            "artist_name": "response",
+            "home_carousel": [
+                {
+                    "id": "d62c6a2a-22ca-4668-b8b9-7534ec32e5bb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png"
+                },
+                {
+                    "id": "3c402628-c55b-42d5-821a-47bf2a58600b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/0.png"
+                },
+                {
+                    "id": "16c0eec6-4ffb-4b72-bc9b-873adeb287b4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/132.png"
+                },
+                {
+                    "id": "2369b5a3-1080-4775-8dc4-d0bfca22dd25",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/188.png"
+                },
+                {
+                    "id": "bf1f6e7e-ee87-4c8a-a39a-a83622739981",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                }
+            ],
+            "art_caption": "family",
+            "art_description": "Fund business on. Already structure tax begin tax would."
+        }
+    ]
+}
